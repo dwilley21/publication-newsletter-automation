e2422117a819e68f982c756c71f6ddaf7fc59a3b
Dalton Willey <daltonwilley@gmail.com>
2025-03-05 06:07:36 -0600

    Add user profile icon to right corner of header

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -7,6 +7,13 @@
         </svg>
         <h1 class="text-xl font-bold text-gray-800">Brevo CSV Processor</h1>
       </div>
+      <div>
+        <button class="text-gray-600 hover:text-gray-800">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+          </svg>
+        </button>
+      </div>
     </div>
   </header>
 </template> 
